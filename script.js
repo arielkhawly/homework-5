c2bd112574efd929fe3b74ebb012fc9006388022
Ariel Khawly <akhawly59@gmail.com>
2020-02-01 13:54:54 -0800

    hour color js and css

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 $(document).ready(function () {
     // DATE CREATION
     let today = new Date(); // set a variable declaring the existence of a date
-    // declare variables for a year, month, and day
-    document.getElementById('date').innerHTML = today;// put the date into the calendar
-// SAVING AND COLOR 
+    document.getElementById('date').innerHTML = today.toDateString();// put the date into the calendar
+    // SAVING AND COLOR 
     $('.hour').each(function () { // set event listener, 
         // 1. check localstorage for the value for each hour
-        let keyName = $(this).children('.hour-btn').text();  
+        let keyName = $(this).children('.hour-btn').text();
         console.log(keyName);
         let everyInput = JSON.parse(localStorage.getItem(keyName));// put JSON here
         console.log(everyInput)
@@ -15,12 +14,24 @@ $(document).ready(function () {
             $(this).children('.user-input').val(everyInput);
         }
 
-        // 2. Assign the color based on current time
+        // 2. Assign the color based on current time (gethour) need 3 conditionals: past, present, future
+        let currentHour = today.getHours() // this is in military time 
+        let hourData = $(this).data('hour');
+        if (currentHour === hourData)
+            $(this).children('.user-input').addClass('current-hour');
+        else if (currentHour > hourData)
+            $(this).children('.user-input').addClass('past-hour');
+        else if (currentHour < hourData)
+        $(this).children('.user-input').addClass('future-hour');
+
+
+
+        // $(this).data('hour'); for retriving html 
 
         // 3. event listener
         let $saveBtn = $(this).children('.save-btn');
-        let hourlyValue = $(this).children('.user-input').val();
         $saveBtn.on('click', function () {
+            let hourlyValue = $(this).children('.user-input').val(); // keep this variable inside, because it will pull the val ON the click
             localStorage.setItem(keyName, JSON.stringify(hourlyValue));// change to 2nd JSON
 
         });
